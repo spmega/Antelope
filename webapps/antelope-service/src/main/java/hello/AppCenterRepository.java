3759c7f8633fe0709fe64d0bb1f65876394c4247
spmega <spmega4567@gmail.com>
2016-06-24 17:16:25 -0400

    Made some changes and deleted unneeded load

--- webapps/antelope-service/src/main/java/hello/AppCenterRepository.java
+++ webapps/antelope-service/src/main/java/hello/AppCenterRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+@RepositoryRestResource(collectionResourceRel = "app", path = "app")
+public interface AppCenterRepository extends PagingAndSortingRepository<Application, Long> {
 
-	List<Person> findByLastName(@Param("name") String name);
+	List<Application> findByName(@Param("name") String name);
 
 }
--- webapps/antelope-service/src/main/java/hello/Application.java
+++ webapps/antelope-service/src/main/java/hello/Application.java
@@ -1,12 +1,33 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@SpringBootApplication
+@Entity
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String name;
+	private String description;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setLastName(String description) {
+		this.description = description;
 	}
 }
